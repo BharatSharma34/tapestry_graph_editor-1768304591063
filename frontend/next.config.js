b99b4655083330ef483deaa200eb07d30630a60c
BharatSharma34 <bharatsharma@ducktaleit.com>
2026-01-13 17:13:12 +0530

    Create frontend/next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/apps/tapestry_graph_editor-1768304591063',
+  publicRuntimeConfig: {
+    basePath: '/apps/tapestry_graph_editor-1768304591063'
+  }
+};
+module.exports = nextConfig;
